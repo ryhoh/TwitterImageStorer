9be359506b776393a54ffd77680c2b3cf23d02be
ryhoh <axis38akasira@gmail.com>
2019-12-16 04:09:46 +0900

    Update run.py
    idsリストのゴミ除去と，ゴミがあっても動くように修正

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import sys
+import traceback
 from crawler import *
 from load_credential import Credential
 
@@ -6,7 +8,12 @@ credit = Credential()
 # ターゲットの Twitter id を改行区切りで記述し id.txt とする
 with open("id.txt", "r") as f:
     ids = f.read().split("\n")
-    ids.remove("")
+
+    if " " in ids:
+        ids.remove(" ")
+
+    if "" in ids:
+        ids.remove("")
 
     # '#' によってコメントアウトされた行を飛ばす
     for uid in ids:
@@ -14,5 +21,12 @@ with open("id.txt", "r") as f:
             ids.remove(uid)
 
 for uid in ids:
-    crawler = Crawler(credit.get_api(), uid)
+    try:
+        crawler = Crawler(credit.get_api(), uid)
+    except ValueError as e:
+        t, v, tb = sys.exc_info()
+        err_name = traceback.format_exception(t, v, tb)[-1].split(':')[0]
+        sys.stderr.write("[{}] skipping '{}'\n".format(err_name, uid))
+        continue
+
     crawler.store_contents()
