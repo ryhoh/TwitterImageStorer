41c48fa7a3233ddc697543ee8602fb470fdefa0d
ryhoh <axis38akasira@gmail.com>
2019-12-16 04:07:22 +0900

    Revert "Update run.py"
    This reverts commit 673998fa6f94354ff5c05b775819d36a098f11c3.

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ credit = Credential()
 # ターゲットの Twitter id を改行区切りで記述し id.txt とする
 with open("id.txt", "r") as f:
     ids = f.read().split("\n")
-    if " " in ids:
-        ids.remove("")
+    ids.remove("")
 
 for uid in ids:
     crawler = Crawler(credit.get_api(), uid)
